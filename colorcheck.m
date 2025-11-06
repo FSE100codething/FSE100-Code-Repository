134928a25dc9e19a7d277e57ea758d90af899985
FSE100codething <jgessing@asu.edu>
2025-11-06 15:42:10 -0700

    Create colorcheck.m

---
+++ colorcheck.m
@@ -0,0 +1,20 @@
+colorcheck = brick.SetColorMode(1,4);
+while 1
+
+if (colorcheck == 5)
+pause(1.0);
+end
+if (colorcheck == 2)
+brick.StopMotor('AD', 'Brake');
+brick.beep();
+brick.beep();
+brick.MoveMotor('AD', 0);
+end
+if (colorcheck == 3)
+brick.StopMotor('AD', 'Brake');
+brick.beep();
+brick.beep();
+brick.beep();
+brick.MoveMotor('AD', 0);
+end
+end
